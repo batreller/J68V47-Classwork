4578910b4e9bf083b48e62330d3d6efa247d7c94
batreller <batreller@gmail.com>
2023-08-30 15:43:09 +0100

    implement Lesson1Exercise1

---
+++ src/Lesson1Exercise1.java
@@ -0,0 +1,9 @@
+public class Lesson1Exercise1 {
+    public static void main(String[] args) {
+        String[] stringArray = {"value1", "value2", "value3"};
+
+        for (String val: stringArray) {
+            System.out.println(val);
+        }
+    }
+}
