31778bcae92bf8b889fa7445bf79b83e10c177b0
Oleksandr <batreller@gmail.com>
2023-09-06 14:56:00 +0100

    implement output as a table

--- src/Lesson2Exercise3.java
+++ src/Lesson2Exercise3.java
@@ -3,27 +3,26 @@ import java.util.Scanner;
 
 public class Lesson2Exercise3 {
     public static void main(String[] args) {
-        HashMap<String, Float> products = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Input name of item " + (i+1) + ": ");
-            String itemName = scanner.nextLine();
-            System.out.print("Input price of " + itemName + ": ");
-            Float itemPrice = Float.parseFloat(scanner.nextLine());
+        System.out.print("Input name of item 1: ");
+        String item1Name = scanner.nextLine();
+        System.out.print("Input price of item 1: ");
+        Float item1Price = Float.parseFloat(scanner.nextLine());
+        System.out.print("Input name of item 2: ");
+        String item2Name = scanner.nextLine();
+        System.out.print("Input price of item 2: ");
+        Float item2Price = Float.parseFloat(scanner.nextLine());
+        System.out.print("Input name of item 3: ");
+        String item3Name = scanner.nextLine();
+        System.out.print("Input price of item 3: ");
+        Float item3Price = Float.parseFloat(scanner.nextLine());
 
-
-            products.put(itemName, itemPrice);
-        }
-
+        int itemNameWidth = 30;
         System.out.println("NESMART");
-        float sum = 0;
-        for (HashMap.Entry<String, Float> entry : products.entrySet()) {
-            String productName = entry.getKey();
-            Float productPrice = entry.getValue();
-            System.out.println(productName + ".........." + "£" + productPrice);
-            sum += productPrice;
-        }
-        System.out.println("SUBTOTAL...." + "£" + sum);
+        System.out.format("%-" + itemNameWidth + "s £%.2f%n", item1Name, item1Price);
+        System.out.format("%-" + itemNameWidth + "s £%.2f%n", item2Name, item2Price);
+        System.out.format("%-" + itemNameWidth + "s £%.2f%n", item3Name, item3Price);
+        System.out.format("SUBTOTAL             £%.2f", (item1Price + item2Price + item3Price));
     }
 }
